a4f3bc870aca3159909a220471bc25ce1f84afb4
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-27 10:47:09 +0200

    added welcome test page

---
+++ pages/Welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container text-left">
+    <div>
+      <h1 class="title">
+        sauna-pwa-app
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+ export default {
+      data () {
+        return {
+            unwatchPushId: null
+        }
+      },
+      mounted () {
+        this.$OneSignal.push(() => console.log(this.$OneSignal))
+      },
+      beforeDestroy () {
+        if (this.unwatchPushId) {
+          this.unwatchPushId()
+          this.unwatchPushId = null
+        }
+      }
+    }
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
