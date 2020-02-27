340a126472c4e5284296e7c7172e1e09c9ca99b0
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-27 13:34:05 +0200

    added onesingnal demo

--- pages/Welcome.vue
+++ pages/Welcome.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="container text-left">
     <div>
-      <h1 class="title">
-        sauna-pwa-app
-      </h1>
+      <h1 class="title">sauna-pwa-app</h1>
     </div>
   </div>
 </template>
 
 <script>
- export default {
-      data () {
-        return {
-            unwatchPushId: null
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    this.$OneSignal.push(() => {
+      this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+        if (isEnabled) {
+          console.log('Push notifications are enabled!')
+        } else {
+          console.log('Push notifications are not enabled yet.')
         }
-      },
-      mounted () {
-        this.$OneSignal.push(() => console.log(this.$OneSignal))
-      },
-      beforeDestroy () {
-        if (this.unwatchPushId) {
-          this.unwatchPushId()
-          this.unwatchPushId = null
-        }
+      })
+    })
+    window.$OneSignal.push([
+      'addListenerForNotificationOpened',
+      (data) => {
+        console.log('Received NotificationOpened:', data)
       }
-    }
+    ])
+  },
+  beforeDestroy() {}
+}
 </script>
 
 <style>
