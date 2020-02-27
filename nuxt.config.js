8e035b10459c15367fdae87eb07ed1b440b8e03d
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-27 14:35:51 +0200

    added firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/onesignal',
     '@nuxtjs/pwa',
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxtjs/firebase',
   ],
   /*
    ** Axios module configuration
@@ -75,6 +76,21 @@ module.exports = {
       }
     }
   },
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_APP_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID
+    },
+    services: {
+      firestore: true,
+      realtimeDb: true
+    }
+  }
   /*
    ** Build configuration
    */
