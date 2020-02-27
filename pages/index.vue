9b77219e36b3394fc3f7ef9bc72ebd619fdc0ef8
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-27 09:44:11 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container text-left">
+    <div>
+      <h1 class="title">
+        sauna-pwa-app
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
