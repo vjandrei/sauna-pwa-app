09a00b191bb062e93ee24b95c6dcaf15ab0b0075
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-28 12:12:46 +0200

    remove firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,8 +49,7 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/onesignal',
     '@nuxtjs/pwa',
-    '@nuxtjs/dotenv',
-    '@nuxtjs/firebase'
+    '@nuxtjs/dotenv'
   ],
   /*
    ** Axios module configuration
@@ -74,21 +73,6 @@ module.exports = {
       welcomeNotification: {
         disable: true
       }
-    }
-  },
-  firebase: {
-    config: {
-      apiKey: process.env.FIREBASE_APP_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
-    },
-    services: {
-      firestore: true,
-      realtimeDb: true
     }
   },
   /*
