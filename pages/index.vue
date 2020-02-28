950a6d88e6fd94a3c6c7b5e847e39dd3ec4799a2
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-28 13:27:08 +0200

    testing

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,46 @@
   <div class="container">
     <div id="data">
       <h1>sauna-pwa-app</h1>
-      <span class="block">Lämpötila: {{ saunaData.temp }} °C</span>
-      <span class="block">Kosteus: {{ saunaData.humidity }} %</span>
+      <span class="block">Lämpötila: -- °C</span>
+      <span class="block">Kosteus: -- %</span>
     </div>
+    {{ todos }}
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.id }}
+    </li>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const config = {
+  apiKey: process.env.FIREBASE_APP_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID
+}
+
+export const db = firebase.initializeApp(config).database()
+
 export default {
   components: {},
   data: () => {
     return {
-      saunaData: []
+      saunaData: [],
+      todos: []
     }
   },
-  mounted() {
-    fetch('https://mokki-sauna.firebaseio.com/data.json').then((response) => {
-      response.json().then((saunaData) => {
-        this.saunaData = saunaData
-      })
-    })
+  props: {
+    database: {}
   },
+  firebase: {
+    database: db.ref('todos').push()
+  },
+  mounted() {},
   methods: {}
 }
 </script>
