6ea55f6582dc260a68de8439f89eff1c759d9f51
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-02-29 10:51:59 +0200

    env var

--- services/fireinit.js
+++ services/fireinit.js
@@ -6,7 +6,7 @@ import 'firebase/firestore'
 import 'firebase/database'
 
 const config = {
-  apiKey: process.env.apiKey,
+  apiKey: process.env.FIREBASE_APP_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
