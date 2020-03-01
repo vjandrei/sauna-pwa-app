fd34925e2f7bef5e36242d88e6c2869d3895052c
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-01 07:01:32 +0200

    confic envs

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,5 +83,15 @@ module.exports = {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  env: {
+    APP_ID: process.env.APP_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_APP_KEY: process.env.FIREBASE_APP_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET
   }
 }
